406acece99900b3cf82fdf2fd5d93fa4f3cc10e4
guilherj <contato.guilhermenascimento@hotmail.com>
2021-05-27 09:38:51 -0300

    Criando Método Listar Clientes

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -6,9 +6,12 @@
 package dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import model.Cliente;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import jdbc.ConnectionFactory;
 
@@ -17,22 +20,22 @@ import jdbc.ConnectionFactory;
  * @author Guilherme
  */
 public class ClienteDAO {
-    
+
     private Connection conn;
-    
-    public ClienteDAO(){
+
+    public ClienteDAO() {
         this.conn = new ConnectionFactory().getConnection();
     }
-    
+
     //Método Cadastrar Cliente
-    public void cadastrarCliente(Cliente obj){
+    public void cadastrarCliente(Cliente obj) {
         try {
-         
-         // 1º Passo - Criar o comando SQL   
-         String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
-                                        + " VALUE(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            
-        // 2º Passo - Conectar o bando de dados e organizar o código
+
+            // 1º Passo - Criar o comando SQL   
+            String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
+                    + " VALUE(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+            // 2º Passo - Conectar o bando de dados e organizar o código
             PreparedStatement st = conn.prepareStatement(sql);
             st.setString(1, obj.getNome());
             st.setString(2, obj.getRg());
@@ -47,28 +50,66 @@ public class ClienteDAO {
             st.setString(11, obj.getBairro());
             st.setString(12, obj.getCidade());
             st.setString(13, obj.getUf());
-            
-        // 3º Passo - Executar o comando SQL    
+
+            // 3º Passo - Executar o comando SQL    
             st.execute();
             st.close();
-            
+
             JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!");
-                      
-         
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
         }
     }
-    
+
     //Método Alterar Cliente
-    public void alterarCliente(){
-        
+    public void alterarCliente() {
+
     }
-    
+
     //Método Excluir Cliente
-    public void excluirCliente(){
-        
+    public void excluirCliente() {
+
     }
-    
+
+    // Método Listar Clientes
+    public List<Cliente> listarClientes() {
+
+        try {
+            List<Cliente> list = new ArrayList<>();
+            String sql = "select * from tb_clientes";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                Cliente obj = new Cliente();
+
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                list.add(obj);
+
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+            return null;
+        }
+
+    }
+
 }
