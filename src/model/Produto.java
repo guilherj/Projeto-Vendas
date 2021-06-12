b32ba927021108fe4b3afbb8e056702153b9c49c
guilherj <contato.guilhermenascimento@hotmail.com>
2021-06-12 13:24:55 -0300

    Criando Classe model.produto

---
+++ src/model/Produto.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Produto {
+    
+    private Integer id;
+    private String descricao;
+    private Double preco;
+    private Integer qtdEstoque;
+    
+    private Fornecedor fornecedor;
+    
+    public Produto(){}
+
+    public Produto(Integer id, String descricao, Double preco, Integer qtdEstoque, Fornecedor fornecedor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.qtdEstoque = qtdEstoque;
+        this.fornecedor = fornecedor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(Integer qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    
+    
+    
+}
